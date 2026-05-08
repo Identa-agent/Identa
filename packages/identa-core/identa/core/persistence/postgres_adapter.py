6bba754a6e1c1feb7e164b54524d0a8776b0e96e
shepax <serodriguezpistelli@gmail.com>
2026-05-07 20:23:01 -0400

    feat(core): add PostgresStorageAdapter and postgres optional dependency

---
+++ packages/identa-core/identa/core/persistence/postgres_adapter.py
@@ -0,0 +1,17 @@
+from identa.core.persistence.sqlite_adapter import SQLiteStorageAdapter, Base
+from sqlalchemy import create_engine
+from sqlalchemy.pool import QueuePool
+from sqlalchemy.orm import sessionmaker
+
+class PostgresStorageAdapter(SQLiteStorageAdapter):
+    def __init__(self, database_url: str, pool_size: int = 5):
+        # Note: We don't call super().__init__ because it initializes a SQLite engine
+        self.engine = create_engine(
+            database_url,
+            poolclass=QueuePool,
+            pool_size=pool_size,
+            max_overflow=10
+        )
+        self._run_migrations()
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
